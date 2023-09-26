3f1af7e163d23477a6fb5107c9bbe166e3a3e14a
Ivan <100620835+foured@users.noreply.github.com>
2023-09-26 10:14:43 +0300

    Update str_easy.h

--- str_easy.h
+++ str_easy.h
@@ -8,5 +8,15 @@ std::string itc_hello_str(std::string name);
 long long itc_len(std::string line);
 void itc_print_copy_str(std::string str, int n);
 void itc_first_end_three(std::string str);
+int itc_count_char_in_str(char ch, std::string str);
+std::string itc_even_place(std::string str);
+double itc_percent_lower_uppercase(std::string str);
+std::string itc_reverse_str(std::string str);
+std::string itc_slice_str(std::string str, int start, int end);
+bool itc_equal_reverse(std::string str);
+std::string itc_cmp_str(std::string str1, std::string str2, int num);
+int itc_find_str(std::string str1, std::string str2);
+std::string itc_three_str(std::string str1, std::string str2, std::string str3);
+int itc_max_char_on_end(std::string str);
 
 #endif
